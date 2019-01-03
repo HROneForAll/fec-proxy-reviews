a91c14e0ac9912160f237f1b9386a765df8b174c
seymakara <seymakara88@gmail.com>
2019-01-03 11:00:01 -0800

    fix the issue with pagination

--- proxy/server.js
+++ proxy/server.js
@@ -10,8 +10,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 
 app.get('/:id/reviews', (req, res) => {
+  var limit = req.query.limit;
+  var offset = req.query.offset;
   request({
-    uri: `http://127.0.0.1:8000/${req.params.id}/reviews`,
+    uri: `http://127.0.0.1:8000/${req.params.id}/reviews?limit=${limit}&offset=${offset}`,
   }, (err, response, body) => {
     if (err) {
       console.log(err);
