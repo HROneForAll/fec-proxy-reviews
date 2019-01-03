6587ca0af912255ffed643b2054fd1e381c1def1
seymakara <seymakara88@gmail.com>
2019-01-02 22:51:39 -0800

    serve review component

---
+++ proxy/server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const morgan = require('morgan');
+const path = require('path');
+const app = express();
+const port = process.env.PORT || 3000;
+const request = require('request');
+
+app.use(morgan('dev'));
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.get('/:id/reviews', (req, res) => {
+  request({
+    uri: `http://127.0.0.1:8000/${req.params.id}/reviews`,
+  }, (err, response, body) => {
+    if (err) {
+      console.log(err);
+    }
+    res.json(JSON.parse(body));
+  })
+})
+
+app.listen(port, () => {
+  console.log(`server running at: http://localhost:${port}`);
+});
